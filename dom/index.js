8040e78fc652bc6f23d4c5c897187c8341549d94
allen <mackey91@gmail.com>
2022-02-06 19:40:10 -0800

    some practice files for students

---
+++ dom/index.js
@@ -0,0 +1,16 @@
+// make sure this file is connected to the index.html 
+
+// wrap all code in this file in an IIFE
+
+
+// get the first div by it's id and log the text inside of it. # do not use querySelector
+
+
+
+// get the second div by it's class name and insert the text "i am cool". # do not use querySelector
+
+
+// with javascript add the class name "sweet" to the element that has an id called div1
+
+
+
