a5879c36aaf71f927dc3ff4cf1d407e6872d1337
allen <mackey91@gmail.com>
2022-02-08 10:27:27 -0800

    change body color with click button and additonal special credit

---
+++ change-body-color/index.js
@@ -0,0 +1,13 @@
+// make sure you follow what's in the html
+// wrap all code in IFFE
+
+// create method changColor and be sure you assigned it to the button in the html
+
+// change the background color of the body to this hex color #03acb1
+// hex must be a string
+
+// **** special credit ****
+
+// create an additonal method called randomColor and when user clicks button the background color of the body
+// changes to a different color
+// it should return a different hex
